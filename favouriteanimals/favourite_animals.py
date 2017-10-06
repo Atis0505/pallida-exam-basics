0ba34631d1049c1b2ee599f91fe4fe937d31ed65
Attila Korom <attilakorom2014@gmail.com>
2017-10-06 10:33:29 +0200

    feat(favourite_animals.py): check user inputs

--- favouriteanimals/favourite_animals.py
+++ favouriteanimals/favourite_animals.py
@@ -7,3 +7,34 @@
 # One animal should be stored only at once
 # Each animal should be written in separate lines
 # The program should only save animals, no need to print them
+
+
+import sys
+
+animals_from_user = sys.argv
+
+def check_user_inputs(user_inputs):
+    if len(user_inputs) == 1:
+        print("fav_animals [animal] [animal]")
+    elif len(user_inputs) == 2:
+        print("Give your favourite animal!")
+    else:
+        list_of_animals = []
+        for i in range(2,len(user_inputs)):
+            list_of_animals.append(user_inputs[i])
+        save_animals(list_of_animals)
+
+
+def save_animals(animals_list):
+    for animal in animals_list:
+        with open("favourites.txt", "r+") as f:
+            for line in f:
+                if animal in line:
+                    break
+            else:
+                f.writelines(animal + "\n")
+        
+                
+
+    
+check_user_inputs(animals_from_user)
