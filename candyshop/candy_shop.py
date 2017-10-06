3aa4e22a25c38ef411c493d88cfceffee3256a59
Attila Korom <attilakorom2014@gmail.com>
2017-10-06 11:18:50 +0200

    feat(candy_shop.py): create attributes and functions for structure of the program

--- candyshop/candy_shop.py
+++ candyshop/candy_shop.py
@@ -16,6 +16,49 @@
 # If we buy sugar we can raise the CandyShop's amount of sugar and reduce the income by the price of it.
 # The CandyShop should be represented as string in this format:
 # "Inventory: 3 candies, 2 lollipops, Income: 100, Sugar: 400gr"
+class Sweet:
+    def __init__(self, sweet_type):
+        self.sweet_type = sweet_type
+        if self.sweet_type == "candy":
+            self.price = 20
+            self.sugar = 10
+        elif self.sweet_type == "lollipop":
+            self.price = 10
+            self.sugar = 5
+
+
+class CandyShop:
+    def __init__(self, amount_of_sugar):
+        self.amount_of_sugar = amount_of_sugar
+        self.money
+        self.lollipops_and_candies = []
+        self.income = 0
+
+
+    def create_sweets(self, kind_of_sweet):
+        sweet = Sweet(kind_of_sweet)
+        self.lollipops_and_candies.append(sweet)
+        self.amount_of_sugar -= sweet.price
+
+
+    def raise_prices(self, precentage):
+        for sweet in self.lollipops_and_candies:
+            sweet.price += sweet.price+precentage
+
+    
+    def sell(self, sweet_type, number_of_amount):
+        for index, sweet in enumerate(self.lollipops_and_candies):
+            if sweet.sweet_type == sweet_type and index <= number_of_amount:
+                del self.lollipops_and_candies[index]
+                self.income += sweet.price
+
+
+    def buy_sugar(self, amount_of_sugar):
+        self.income -= amount_of_sugar/10
+
+    
+
+
 
 candy_shop = CandyShop(300)
 candy_shop.create_sweets("candy")
