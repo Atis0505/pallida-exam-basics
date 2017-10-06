f83547fb8ed78002af1af454181cd932e5d7e3fa
Attila Korom <attilakorom2014@gmail.com>
2017-10-06 09:30:40 +0200

    feat(unique_chars.py):create dictionary for letters

--- uniquechars/unique_chars.py
+++ uniquechars/unique_chars.py
@@ -1,6 +1,19 @@
 # Create a function called `unique_characters` that takes a string as parameter
 # and returns a list with the unique letters of the given string
 # Create basic unit tests for it with at least 3 different test cases
+
+def unique_characters(input_string):
+    letters_dict = {}
+    for letter in input_string:
+        if letter in letters_dict:
+            letters_dict[letter] += 1
+        else:
+            letters_dict[letter] = 1
+    
+    return unique_chars_list
+
 print(unique_characters("anagram"))
 # Should print out:
 # ["n", "g", "r", "m"]
+
+
